e3a5e56577414dd81e35164d3299f59301706e60
Adam Kasztenny <adamkasztenny@gmail.com>
2020-10-03 21:36:40 -0400

    Adds EDIFACT envelope file

--- generate-schemas.sh
+++ generate-schemas.sh
@@ -3,9 +3,34 @@
 OUTPUT_ROOT="$(pwd)"
 echo "Outputting to $OUTPUT_ROOT"
 
+function convert_file() {
+	input_file=$1
+	output_file=$2
+
+	mkdir -p "$(dirname $output_file)"
+	echo "Writing to $output_file"
+
+	input_file_without_comments=$(cat "$input_file" | sed '/^\s*#/d')
+	input_file_without_python_none=$(echo "$input_file_without_comments" | sed 's/None/null/g')
+
+	if [[ $output_file == *"records"* ]]; then
+		json_without_proper_quotes="{$(echo "$input_file_without_python_none" | sed '1,/recorddefs/d')"
+	else
+		json_without_proper_quotes="[$(echo "$input_file_without_python_none" | sed '1,/structure/d')"
+	fi
+
+
+	json_with_trailing_commas="$(echo "$json_without_proper_quotes" | sed "s/'/\"/g")"
+	echo "var raw = $json_with_trailing_commas; console.log(JSON.stringify(raw));" > script.js
+	as_valid_json=$(node script.js)
+
+	echo "$as_valid_json" > "$output_file"
+}
+
 function convert() {
 	edi=$1
 	cd $edi
+
 	for version in `ls`
 	do
 		output_directory="$OUTPUT_ROOT/$edi"
@@ -13,23 +38,11 @@ function convert() {
 		for file in `ls $version/*`
 		do
 			output_file="$output_directory/$(echo $file | rev | cut -c4- | rev).json"
-			mkdir -p "$(dirname $output_file)"
-			echo "Writing to $output_file"
-
-			if [[ $output_file == *"records"* ]]; then
-				json_without_proper_quotes="{$(cat "$file" | sed '1,/recorddefs/d')"
-			else
-				json_without_proper_quotes="[$(cat "$file" | sed '1,/structure/d')"
-			fi
-
-
-			json_with_trailing_commas="$(echo "$json_without_proper_quotes" | sed "s/'/\"/g")"
-			echo "var raw = $json_with_trailing_commas; console.log(JSON.stringify(raw));" > script.js
-			as_valid_json=$(node script.js)
-
-			echo "$as_valid_json" > "$output_file"
+			convert_file "$file" "$output_file"
 		done
 	done
+
+	cd ..
 }
 
 git clone https://github.com/bots-edi/bots-grammars
@@ -37,6 +50,8 @@ cd bots-grammars
 
 convert "x12"
 convert "edifact"
+
+convert_file "edifact/envelope.py" "$OUTPUT_ROOT/edifact/envelope.json"
 
 cd ..
 rm -rf bots-grammars
